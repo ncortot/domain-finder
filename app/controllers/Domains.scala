1e6dcab3c3c0fa522d07be6379395b4d93844091
Nicolas Cortot <nc@rznc.net>
2014-10-16 01:15:44 +0200

    Generator refactoring

--- app/controllers/Domains.scala
+++ app/controllers/Domains.scala
@@ -2,16 +2,11 @@ package controllers
 
 import javax.inject.Singleton
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
-import play.api.libs.iteratee.Enumerator
 import play.api.libs.json.Json
 import play.api.mvc.Action
 import play.api.mvc.Controller
-import play.modules.reactivemongo.json.collection.JSONCollection
-import reactivemongo.api.bulk
-import scala.concurrent.Future
 
 import models.Domain
-import models.Token
 
 @Singleton
 class Domains extends Controller with MongoAPI[Domain] {
@@ -47,49 +42,6 @@ class Domains extends Controller with MongoAPI[Domain] {
       .map { lastError =>
         Ok(s"Object updated")
       }
-  }
-
-  private def getDomains: Future[List[String]] = collection
-    .find(Json.obj())
-    .cursor[Domain]
-    .collect[List]()
-    .map { objects => objects map { _.name } }
-
-  private def getTokens: Future[List[String]] = db
-    .collection[JSONCollection](Token.collectionName)
-    .find(Json.obj())
-    .cursor[Token]
-    .collect[List]()
-    .map { objects => objects map { _.value } }
-
-  private def generateDomains(tokenA: String, tokenB: String): List[String] =
-    List(tokenA + tokenB + ".com")
-
-  private def generateDomains(tokens: List[String]): List[String] =
-    for {
-      tokenA <- tokens
-      tokenB <- tokens
-      domain <- generateDomains(tokenA, tokenB)
-    } yield domain
-
-  private def generateDomains: Future[List[String]] =
-    for {
-      knownDomains <- getDomains
-      knownTokens <- getTokens
-    } yield (generateDomains(knownTokens).toSet -- knownDomains.toSet).toList
-
-  def generate = Action.async {
-    generateDomains.flatMap {
-      case Nil =>
-        Future.successful(Ok("No new domain"))
-      case newDomains =>
-        val objects = newDomains.map { name => Domain(name = name, score = 0) }
-        collection
-          .bulkInsert(Enumerator.enumerate(objects))
-          .map { lastError =>
-            Ok(s"${newDomains.size} new domains generated")
-          }
-    }
   }
 
 }
--- app/controllers/Generator.scala
+++ app/controllers/Generator.scala
@@ -1,8 +1,63 @@
 package controllers
 
-/**
- * Created by nc on 10/16/14.
- */
-class Generator {
+import javax.inject.Singleton
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.libs.iteratee.Enumerator
+import play.api.libs.json.Json
+import play.api.mvc.Action
+import play.api.mvc.Controller
+import play.modules.reactivemongo.json.collection.JSONCollection
+import scala.concurrent.Future
+
+import models.Domain
+import models.Token
+
+@Singleton
+class Generator extends Controller with MongoController {
+
+  private def getDomains: Future[List[String]] = db
+    .collection[JSONCollection](Domain.collectionName)
+    .find(Json.obj())
+    .cursor[Domain]
+    .collect[List]()
+    .map { objects => objects map { _.name } }
+
+  private def getTokens: Future[List[String]] = db
+    .collection[JSONCollection](Token.collectionName)
+    .find(Json.obj())
+    .cursor[Token]
+    .collect[List]()
+    .map { objects => objects map { _.value } }
+
+  private def generateDomains(tokenA: String, tokenB: String): List[String] =
+    List(tokenA + tokenB + ".com")
+
+  private def generateDomains(tokens: List[String]): List[String] =
+    for {
+      tokenA <- tokens
+      tokenB <- tokens
+      domain <- generateDomains(tokenA, tokenB)
+    } yield domain
+
+  private def generateDomains: Future[List[String]] =
+    for {
+      knownDomains <- getDomains
+      knownTokens <- getTokens
+    } yield (generateDomains(knownTokens).toSet -- knownDomains.toSet).toList
+
+  def generate = Action.async {
+    generateDomains.flatMap {
+      case Nil =>
+        Future.successful(Ok("No new domain"))
+      case newDomains =>
+        val collection = db.collection[JSONCollection](Domain.collectionName)
+        val objects = newDomains.map { name => Domain(name = name, score = 0) }
+        collection
+          .bulkInsert(Enumerator.enumerate(objects))
+          .map { lastError =>
+            Ok(s"${newDomains.size} new domains generated")
+          }
+    }
+  }
 
 }
